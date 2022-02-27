f210431833d2dfe5796260091082f69c9761f488
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-02-27 21:38:20 +0200

    Done SignUp using Axios 'database updated'

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -9,32 +9,21 @@ import axios from "axios";
 function SignUpForm() {
 
     const SignUpURL = "http://127.0.0.1:8000/api/register";
-    const [email,setEmail] = useState(null);
-    const [password,setPassword] = useState(null);
-    const [name,setName] = useState(null);
-    const [confirmPassword,setConfirmPassword] = useState(null);
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [password_confirmation,setPassword_confirmation] = useState("");
 
-  React.useEffect(() => {
-    axios.get(`${SignUpURL}/1`).then((response) => {
-        setEmail(response.data);
-        setPassword(response.data);
-        setName(response.data);
-        setConfirmPassword(response.data);
-         });
-        }, []);
-
-
-    function signUP() {
+    function signUP(event) {
+        event.preventDefault();
+        let data = {name,email,password,password_confirmation};
         axios
-          .post(SignUpURL, {
-            title: "Hello World!",
-            body: "This is a new post."
-          })
-          .then((response) => {
-            setEmail(response.data);
+          .post(SignUpURL, data).then((response) => {
+           /* setEmail(response.data);
             setPassword(response.data);
             setName(response.data);
-            setConfirmPassword(response.data);
+            setConfirmPassword(response.data);*/
+            console.log("test")
           });
       }
                
@@ -62,7 +51,7 @@ function SignUpForm() {
                   <TextField  type="password" placeholder="Enter Password" label="password" variant="outlined" value={password} onInput={e => setPassword(e.target.value)} fullWidth required />
                 </Grid>
                 <Grid item xs={12}>
-                  <TextField  type="password" placeholder="Confirm Password" label="Confirm password" variant="outlined" value={confirmPassword} onInput={e => setConfirmPassword(e.target.value)} fullWidth required />
+                  <TextField  type="password" placeholder="Confirm Password" label="Confirm password" variant="outlined" value={password_confirmation} onInput={e => setPassword_confirmation(e.target.value)} fullWidth required />
                 </Grid>
                 <Grid item xs={12}>
                   <Button type="submit" variant="contained" color="primary" onClick= {signUP} fullWidth>SignUp</Button>
