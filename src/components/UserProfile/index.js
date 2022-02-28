f62314288bfa970cb9e2f99dd0428ea4da73622d
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-02-28 22:41:01 +0200

    uninstall package '@mui/styles' cause of conflicts

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect} from 'react';
 import ContactUs from "../../pages/ContactUs";
 //import { Navigate } from 'react-router-dom';
 import { renderMatches, useNavigate } from 'react-router-dom';
-import { Grid, TextField, Button, Card, CardContent, Typography } from '@mui/material/';
 //import { RouteComponentProps } from "react-router-dom";
 import AuthService from "../../Services/AuthService";
 import axios from "axios";
 import Cookie from "universal-cookie";
+//import {Tabs, Tab, Col, Row}  from 'react-bootstrap';
+
+
 
 
 function UserProfile() {
@@ -34,20 +36,10 @@ function UserProfile() {
 
         fetchData();
      
-
-
     
       return (
-        <div>
-            <ul> 
-                <li>{name}</li>
-             
-             <li>
-               {email}
-             </li>
+          <h1>hi</h1>
 
-            </ul>
-        </div>
       )
     
      /* const fetchData = async () => {
