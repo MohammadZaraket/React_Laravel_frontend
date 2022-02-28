8ba029fa64a1b59fb1439ec5246ae19a231266e6
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-02-28 18:35:28 +0200

    Get the cookie from browser

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -10,38 +10,35 @@ import Cookie from "universal-cookie";
 
 
 function UserProfile() {
+      const [user, setUsers] = useState([])
+ 
 
 
-  return (
-    <div className="App"> 
-      <Grid>
-        <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
-          <CardContent>
-            <Typography gutterBottom variant="h5" color="blue">
-              SignIn
-          </Typography> 
-            <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
-              So You Can Access The Dashboard To Edit Profile
-          </Typography> 
-            <form>
-              <Grid container spacing={1}>
-                <Grid item xs={12}>
-                  <TextField type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField  type="password" placeholder="Enter Password" label="password" variant="outlined" fullWidth required />
-                </Grid>
-                <Grid item xs={12}>
-                  <Button type="submit" variant="contained" color="primary" fullWidth>SignIn</Button>
-                </Grid>
+        const cookies = new Cookie();
+        const [cookie, setCookie] = useState(cookies.get('access_token'))  
+    
+     /* const fetchData = async () => {
+        const response = await fetch("http://127.0.0.1:8000/api/user-profile")
+        const data = await response.json()
+        setUsers(data)
+      }
+    
+      useEffect(() => {
+        fetchData()
+      }, [])
+         <li key={user.id}>{user.name}</li>*/
+    
+      return (
+        <div>
+            <ul>
+             <li>
+                 Test
+                 {cookie}
+             </li>
 
-              </Grid>
-            </form>
-          </CardContent>
-        </Card>
-      </Grid>
-    </div>
-  );
+            </ul>
+        </div>
+      )
 }
 
 export default UserProfile;
